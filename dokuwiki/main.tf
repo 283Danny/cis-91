e2ddff1ef2c519f6017c168f4550dd18ee02f651
Dan Fulton <aquabluecis75@gmail.com>
2022-12-10 22:02:00 +0000

    Dkuwiki

--- dokuwiki/main.tf
+++ dokuwiki/main.tf
@@ -32,11 +32,11 @@ provider "google" {
 }
 
 resource "google_compute_network" "vpc_network" {
-  name = "cis91-network"
+  name = "dokuwiki-network"
 }
 
 resource "google_compute_instance" "vm_instance" {
-  name         = "cis91"
+  name         = "dokuwiki"
   machine_type = "e2-micro"
 
   boot_disk {
@@ -50,10 +50,19 @@ resource "google_compute_instance" "vm_instance" {
     access_config {
     }
   }
+  attached_disk{
+    source = goggle_compute_disk.dokuwiki8.self_link
+    device_name = "dokuwiki8"
+  }
+  service_account {
+    email  = google_service_account.lab08-service-account.email
+    scopes = ["cloud-platform"]
+  }
+
 }
 
 resource "google_compute_firewall" "default-firewall" {
-  name = "default-firewall"
+  name = "dokuwiki-firewall"
   network = google_compute_network.vpc_network.name
   allow {
     protocol = "tcp"
@@ -61,6 +70,59 @@ resource "google_compute_firewall" "default-firewall" {
   }
   source_ranges = ["0.0.0.0/0"]
 }
+
+  
+resource "google_service_account" "lab08-service-account" {
+  account_id   = "lab08-service-account"
+  display_name = "lab08-service-account"
+  description = "Service account for lab 08"
+}
+
+
+resource "google_project_iam_member" "project_member" {
+  role = "roles/storage.objectAdmin"
+  member = "serviceAccount:${google_service_account.lab08-service-account.email}"
+}
+
+resource "google_storage_bucket" "dan-bucket8" {
+  name          = "dan-bucket8"
+  location      = "US"
+  storage_class = "multi_regional"
+
+  lifecycle_rule {
+    condition {
+      age = 180
+    }
+    action {
+      type = "Delete"
+    }
+  }  
+}
+
+resource "google_compute_disk" "dokuwiki8" {
+  name  = "dokuwiki8"
+  type  = "pd-ssd"
+  labels = {
+    environment = "dev"
+  }
+  size = "10"
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 output "external-ip" {
   value = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
